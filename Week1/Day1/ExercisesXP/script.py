a8268450a72a9fa453218cbed5c8fc56ce63d568
David-7805 <davidsmits@aol.nl>
2024-03-03 13:28:28 +0200

    second push of first exercise

--- Week1/Day1/ExercisesXP/script.py
+++ Week1/Day1/ExercisesXP/script.py
@@ -1,1 +1,17 @@
 print("Hello world\nHello world\nHello world\nHello world")
+
+some_math = (99 ** 3) * 8
+print(some_math)
+
+def predict(statement, prediction):
+    if statement == prediction:
+        print(f"{prediction} was the right prediction")
+
+predict(5 < 3,False)
+predict(3 == 3, True)
+predict(3 == "3", False)
+# "3" > 3 It is impossible to perform the greater than operator on a string and an integer
+predict("Hello" == "hello", False)
+
+computer_brand = "Acer"
+print(f'I have an {computer_brand} computer')
