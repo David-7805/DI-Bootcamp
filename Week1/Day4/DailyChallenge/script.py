fd25435d67af061a6a2836a0ef23f34713ea76de
David-7805 <davidsmits@aol.nl>
2024-03-06 21:21:16 +0200

    Hopefully final push of DailyChallenge of day 4

--- Week1/Day4/DailyChallenge/script.py
+++ Week1/Day4/DailyChallenge/script.py
@@ -7,9 +7,7 @@ def from_string_to_grid(string, grid_width):
     substring = string[iteration * grid_width: (iteration + 1) * grid_width]
     if substring != '':
       list_of_substrings.append(substring)
-  print(list_of_substrings)
-
-from_string_to_grid(test_string, 3)
+  return list_of_substrings
 
 def vertical_reader(grid, index):
   vertical_string = ''
@@ -22,8 +20,20 @@ def combiner_of_vertical_strings(grid):
   substring_length = len(grid[0])
   for position in range(substring_length):
     result += vertical_reader(grid, position)
-  print(result)
+  return result
 
-combiner_of_vertical_strings(['7ii', 'Tsx', 'h%?', 'i #', 'sM ', '$a ', '#t%', '^r!'])
-# vertical_reader(['7ii', 'Tsx', 'h%?', 'i #', 'sM ', '$a ', '#t%', '^r!'], 0)
+def only_alpha_characters(string):
+  new_string = ''
+  for character in string:
+    if 65 <= ord(character) <= 91 or 97 <= ord(character) <= 123:
+      new_string += character
+    else:
+      new_string += ' '
+  list_of_the_words = new_string.split()
+  string_to_output = ' '.join(list_of_the_words)
+  print(string_to_output)
+  return string_to_output
 
+grid = from_string_to_grid(test_string, 3)
+possible_message = combiner_of_vertical_strings(grid)
+only_alpha_characters(possible_message)
