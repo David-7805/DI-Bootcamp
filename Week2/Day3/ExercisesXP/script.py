afafcb4920c2def014b7f8fd5a518fd9d28cc759
David-7805 <davidsmits@aol.nl>
2024-03-12 21:59:46 +0200

    Another push of ExercisesXP week 2, day 3

--- Week2/Day3/ExercisesXP/script.py
+++ Week2/Day3/ExercisesXP/script.py
@@ -1,7 +1,7 @@
 from string import ascii_letters
 from random import choices
 import datetime
-from faker import Faker
+# from faker import Faker
 
 # Exercise 1: Currencies
 
@@ -14,7 +14,7 @@ class Currency:
         return f'{self.amount} {self.currency}s'
 
     def __int__(self):
-        return int(self.amount)
+        return self.amount
 
     def __repr__(self):
         return f'{self.amount} {self.currency}s'
@@ -45,17 +45,17 @@ c2 = Currency('dollar', 10)
 c3 = Currency('shekel', 1)
 c4 = Currency('shekel', 10)
 
-print('str(c1):', str(c1))
-print('int(c1):', int(c1))
-print('repr(c1):', repr(c1))
-print('c1 + 5:', c1 + 5)
-print('c1 + c2:', c1 + c2)
-print('original c1:', c1)
+print(str(c1))
+print(int(c1))
+print(repr(c1))
+print(c1 + 5)
+print(c1 + c2)
+print(c1)
 c1 += 5
-print('c1 after 5 added:', c1)
+print(c1)
 c1 += c2
-print('c1 after c2 added:', c1)
-# print('c1 + c3:', c1 + c3)
+print(c1)
+# print(c1 + c3)
 
 # Exercise 3: String Module
 # 1.
@@ -76,9 +76,9 @@ def time_to_new_year(year):
   any_new_year = f'{year}-01-01 00:00:00'
   any_new_year_datetime_object = datetime.datetime.strptime(any_new_year, "%Y-%m-%d %H:%M:%S")
   now = datetime.datetime.now()
-  time_to_new_year = str(any_new_year_datetime_object - now)
+  time_to_new_year = str(any_new_year_datetime_object - now) # returns a string like '294 days, 2:09:26.596043'
   list_of_time_to_new_year = time_to_new_year.split(', ')
-  list_of_time_to_new_year[1] = list_of_time_to_new_year[1].split('.')
+  list_of_time_to_new_year[1] = list_of_time_to_new_year[1].split('.') # I want to get rid of the decimals after the '.'
   print(f'The 1st January of {year} is in {list_of_time_to_new_year[0]} and {list_of_time_to_new_year[1][0]} hours.')
 
 time_to_new_year(2025)
@@ -89,8 +89,7 @@ def minutes_lived(birth_year, birth_month, birth_day):
     birthdate = datetime.date(birth_year, birth_month, birth_day)
     current_date = datetime.date.today()
     time_lived = current_date - birthdate
-    list_of_time_lived = str(time_lived).split(' ')
-    print(list_of_time_lived)
+    list_of_time_lived = str(time_lived).split(' ') # String '16739 days, 0:00:00' is being split, I only need the first part
     days_lived = int(list_of_time_lived[0])
     minutes_lived = days_lived * 24 * 60
     print(f"You have lived approximately {minutes_lived} minutes until this date.")
@@ -98,7 +97,7 @@ def minutes_lived(birth_year, birth_month, birth_day):
 minutes_lived(1978, 5, 14)
 
 # Exercise 7 : Faker Module
-fake = Faker()
+# fake = Faker()
 users = []
 
 
