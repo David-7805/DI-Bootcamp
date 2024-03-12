65e48979709a54103c66c5c24186c471b5a22c00
David-7805 <davidsmits@aol.nl>
2024-03-12 19:28:54 +0200

    First push of DailyChallenge week 2, day 3

---
+++ Week2/Day3/DailyChallenge/script.py
@@ -0,0 +1,51 @@
+from math import pi
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+        self.diameter = self.radius * 2
+
+    def area(self):
+        return pi * self.radius ** 2
+
+    def __str__(self):
+        return f'The circle has a radius of {self.radius} and a diameter of {self.diameter}.'
+
+    def __add__(self, another_circle):
+        area_new_circle = self.area() + another_circle.area()
+        radius_new_circle = (area_new_circle / pi) ** 0.5
+        return Circle(radius_new_circle)
+
+    def __gt__(self, another_circle):
+        return self.radius > another_circle.radius
+
+    def __eq__(self, another_circle):
+        return self.radius == another_circle.radius
+
+    def sort_circles(self, *circles):
+        list_of_circles_by_radius = [(self.radius, self)]
+        for circle in circles:
+            list_of_circles_by_radius.append((circle.radius, circle))
+        list_of_circles_by_radius.sort()
+        return list_of_circles_by_radius
+
+
+
+circle_1 = Circle(4)
+print(circle_1)
+circle_2 = Circle(3)
+print(circle_2)
+circle_3 = circle_1 + circle_2
+print(circle_3)
+print(circle_1 > circle_2)
+print(circle_1 == circle_2)
+circle_4 = Circle(4)
+print(circle_1.sort_circles(circle_2, circle_3, circle_4))
+list_of_circles = [circle_1]
+
+
+
+
+
+
+
