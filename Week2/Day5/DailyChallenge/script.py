b62370ba8ec1ea32520bbf094cfcfcda0d036ab4
David-7805 <davidsmits@aol.nl>
2024-03-16 20:24:54 +0200

    Hopefully final push of DailyChallenge of week 2, day 5

--- Week2/Day5/DailyChallenge/script.py
+++ Week2/Day5/DailyChallenge/script.py
@@ -1,5 +1,32 @@
 import random
 
+# Part 1: Quizz:
+# * What is a class? A class is a blueprint that defines which attributes and methods a future object of that class
+#   will have.
+# * What is an instance? An instance of a class is an object that was instantiated by using the name of the class and
+#   if required also parameters that will be the values of the attributes of the object.
+# * What is encapsulation? Encapsulation is the bundling of attributes and methods within a class. It is also the
+#   possibility to have _protected and __private attributes and methods within a class. _protected attributes/methods
+#   signal to the one who uses them that it is better not to change them, while __private attributes/methods are not
+#   visible to the programmer and have a more complicated way to access them, like object_name._ClassName__attribute.
+# * What is abstraction? Abstraction is the possibility to use class methods (and attributes) just by typing a period
+#   after the object name without having to know how they are programmed.
+# * What is inheritance? Inheritance is the idea that classes can have relations between them. With inheritance a class
+#   can be a child of another class and inherit attributes and methods from that parent class, in case they are not
+#   defined in the child class itself.
+# * What is multiple inheritance? Multiple inheritance is the possibility that a class can inherit attributes and
+#   methods from more than only one parent class. In that case the interpreter looks for attributes and methods in an
+#   order that is named method resolution order or MRO, see the last question of the quiz.
+# * What is polymorphism? Polymorphism is the fact that a function can be defined in which a method is used that two
+#   different classes both have, even though these methods do different things dependent on the class the object
+#   belongs to.
+# * What is method resolution order or MRO? Method resolution order defines the order in which the interpreter will
+#   look for methods and attributes of an object in case it belongs to a class that inherits from more than one parent
+#   class and also from grandparent classes. The order is depth-first left-to-right, meaning that the interpreter first
+#   looks in the class itself, than in its parents in the order in which they were written while creating the class,
+#   and then in its grandparents.
+
+# Part 2: Create A Deck Of Cards Class
 class Card:
     def __init__(self, suit, value):
         self.suit = suit
